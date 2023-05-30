62093d8f0184aa86a44ba43cbf0dc54a0956d0ed
hirwajeaneric <hirwaminerve25@gmail.com>
2023-05-30 17:22:04 +0200

    Start of routing system

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import NavigationBar from '../components/sections/NavigationBar';
+import Footer from '../components/sections/Footer';
+import { Outlet } from 'react-router-dom';
 
 export default function index() {
   return (
     <>
         <NavigationBar />
+        <Outlet />
+        <Footer />
     </>
   )
 }
