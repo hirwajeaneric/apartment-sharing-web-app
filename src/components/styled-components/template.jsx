bfee82574739625db8a110cb74b8e7cfc7374e4b
hirwajeaneric <hirwaminerve25@gmail.com>
2023-05-30 12:24:29 +0200

    First commit

---
+++ src/components/styled-components/template.jsx
@@ -0,0 +1,61 @@
+import styled from 'styled-components';
+
+export const a = styled.div`
+
+    @media (max-width: 1080px) {
+            
+    }
+
+    @media (max-width: 768px) {
+        
+    }
+
+    @media (max-width: 480px) {
+        
+    }
+`;
+
+export const b = styled.div`
+
+    @media (max-width: 1080px) {
+            
+    }
+
+    @media (max-width: 768px) {
+        
+    }
+
+    @media (max-width: 480px) {
+        
+    }
+`;
+
+export const J = styled.div`
+
+    @media (max-width: 1080px) {
+            
+    }
+
+    @media (max-width: 768px) {
+        
+    }
+
+    @media (max-width: 480px) {
+        
+    }
+`;
+
+export const K = styled.div`
+
+    @media (max-width: 1080px) {
+            
+    }
+
+    @media (max-width: 768px) {
+        
+    }
+
+    @media (max-width: 480px) {
+        
+    }
+`;
