418743142046a71b3ec4b5cf66819d4a90622990
hirwajeaneric <hirwaminerve25@gmail.com>
2023-05-31 12:39:16 +0200

    Added model for Mobile Nav

--- src/components/sections/NavigationBar.jsx
+++ src/components/sections/NavigationBar.jsx
@@ -1,28 +1,42 @@
 import React, { useEffect } from 'react'
 import { FullWidthContainer, PageSizedContainer, TwoSidedContainer } from '../styled-components/generalComponents'
 import { MdOutlineApartment } from 'react-icons/md';
-import { NavLink, useNavigate, useParams } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import { IconButton, Menu, MenuItem } from '@mui/material';
-import { AccountCircle } from '@mui/icons-material';
+import { AccountCircle, Close, Menu as MenuIcon } from '@mui/icons-material';
 import { useState } from 'react';
-import { DesktopNav, Logo } from '../styled-components/navigationStyledComponents';
+import { DesktopNav, Logo, MobileNav, MobileNavButton } from '../styled-components/navigationStyledComponents';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
 export default function NavigationBar() {
   const [anchorEl, setAnchorEl] = useState(null);
   const navigate = useNavigate();
-  const params = useParams();
+  const [open, setOpen] = React.useState(false);
+  const [userInfo, setUserInfo] = useState({ id: '', email: '', fullName: '', phone: '', nationality: '', nationalId: '', passportNumber: '', profilePicturer: '' });
+  const [openNav, setOpenNav] = useState(false);
 
-  const [userInfo, setUserInfo] = useState({
-    id: '',
-    email: '',
-    fullName: '',
-    phone: '',
-    nationality: '',
-    nationalId: '',
-    passportNumber: '',
-    profilePicturer: '',
-  });
-  
+  const handleOpenNav = () => setOpen(true);
+  const handleCloseNav = () => setOpen(false);
+
+  const toggleMenu = () => {
+    setOpenNav(!openNav);
+  }
+
   useEffect(()=> {
     setUserInfo(JSON.parse(localStorage.getItem('usrInfo')));
     // setUserInfo(localStorage.getItem('usrInfo'));
@@ -57,7 +71,7 @@ export default function NavigationBar() {
   return (
     <FullWidthContainer style={{ background: 'white' }}>
       <PageSizedContainer>
-        <TwoSidedContainer>
+        <TwoSidedContainer style={{ position: 'relative' }}>
           <Logo to={'/'} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', gap: '5px', fontWeight: '700'}}>
             <MdOutlineApartment /> ISMA
           </Logo>
@@ -87,6 +101,42 @@ export default function NavigationBar() {
               </div>
             }
           </DesktopNav>
+
+
+          {/* Mobile navigation  */}
+          <MobileNavButton onClick={handleOpenNav}>
+            {open ? <Close /> : <MenuIcon />}
+          </MobileNavButton>
+          <Modal open={open} onClose={handleCloseNav} aria-labelledby="modal-nav" aria-describedby="modal-modal-description">
+            <Box sx={style}>
+              <MobileNav>
+                <NavLink to={'/'}>Home</NavLink>
+                <NavLink to={'/aboutus'}>About Us</NavLink>
+                <NavLink to={'/contactus'}>Contact Us</NavLink>
+                <NavLink to={'/post'}>Post New Property</NavLink>
+                {localStorage.getItem('usrTkn') &&
+                  <NavLink to={`/user/${userInfo.fullName.split(' ').join('')}`}>My Account</NavLink>
+                }
+                {!localStorage.getItem('usrTkn') && 
+                  <>
+                    <button type='button' onClick={signin}>Sign In</button>
+                    <button type='button' onClick={signup}>Sign Up</button>
+                  </>
+                }
+                {localStorage.getItem('usrTkn') && 
+                  <div>
+                    <IconButton size="large" sx={{ padding: '0px' }} aria-label="account of current user" aria-controls="menu-appbar" aria-haspopup="true" onClick={handleMenu} color="inherit">
+                      <AccountCircle style={{ color: 'green' }}/>
+                    </IconButton>
+                    <Menu id="menu-appbar" anchorEl={anchorEl} anchorOrigin={{ vertical: 'top', horizontal: 'right' }} keepMounted transformOrigin={{ vertical: 'top', horizontal: 'right' }} open={Boolean(anchorEl)} onClose={handleClose}>
+                      <MenuItem onClick={() => { navigate(`/user/${userInfo.fullName.split(' ').join('')}/settings`); handleClose(); }}>Profile/Settings</MenuItem>
+                      <MenuItem onClick={() => { signout(); handleClose(); }}>Log out</MenuItem>
+                    </Menu>
+                  </div>
+                }
+              </MobileNav>
+            </Box>
+          </Modal>
         </TwoSidedContainer>
       </PageSizedContainer>
     </FullWidthContainer>
