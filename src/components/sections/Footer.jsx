e58a9ec058a4409ec99e51ff8a5ec1b0b535acfc
hirwajeaneric <hirwaminerve25@gmail.com>
2023-06-01 11:13:22 +0200

    Footer completed

--- src/components/sections/Footer.jsx
+++ src/components/sections/Footer.jsx
@@ -6,20 +6,20 @@ export default function Footer() {
   return (
     <FullWidthContainer style={{ background: 'white' }}>
         <PageSizedContainer style={{ flexDirection: 'column', alignItems: 'center' }}>
-            <TwoSidedContainer>
-              <LeftContainer style={{ background: 'green', flexDirection: 'column', alignItems: 'flex-start' }}>
-                <div style={{ display: 'flex', flexDirection: 'column', justifyContent:'flex-start', alignItems: 'flex-start', width: '100%' }}>
-                  <h3>Useful Links</h3>
-                  <Link to={''}>Home</Link>
-                  <Link to={''}>About Us</Link>
-                  <Link to={''}>Contact Us</Link>
-                  <Link to={''}>Post new</Link>
+            <TwoSidedContainer style={{ margin: '40px 0', padding: '0 10px' }}>
+              <LeftContainer style={{ flexDirection: 'column', alignItems: 'flex-start' }}>
+                <div style={{ display: 'flex', flexDirection: 'column', justifyContent:'flex-start', alignItems: 'flex-start', width: '100%', marginBottom: '20px' }}>
+                  <h3 style={{ marginBottom: '20px' }}>Useful Links</h3>
+                  <Link to={'/'} style={{ color: 'black', textDecoration: 'none', lineHeight: '25px' }}>Home</Link>
+                  <Link to={'/aboutus'} style={{ color: 'black', textDecoration: 'none', lineHeight: '25px' }}>About Us</Link>
+                  <Link to={'/contactus'} style={{ color: 'black', textDecoration: 'none', lineHeight: '25px' }}>Contact Us</Link>
+                  <Link to={'/post'} style={{ color: 'black', textDecoration: 'none', lineHeight: '25px' }}>Post new</Link>
                 </div>
               </LeftContainer>
-              <RightContainer style={{ background: 'orange', flexDirection: 'column', alignItems: 'flex-start' }}>
+              <RightContainer style={{ flexDirection: 'column', alignItems: 'flex-start' }}>
                 <div style={{ display: 'flex', flexDirection: 'column', justifyContent:'flex-start', alignItems: 'flex-start', width: '100%' }}>
-                  <h3>Contact Us</h3>
-                  <p>
+                  <h3 style={{ marginBottom: '20px' }}>Contact Us</h3>
+                  <p style={{ lineHeight: '25px'}}>
                     ISMA Ltd<br/>
                     KG 541 ST, House No 10<br/>
                     P.O.Box 3009 Kigali<br/>
@@ -29,9 +29,7 @@ export default function Footer() {
                 </div>
               </RightContainer>
             </TwoSidedContainer>
-            <p 
-              style={{ padding: '40px 20px' }}
-            >© ISMA Ltd {new Date().getFullYear()}. All Rights Reserved.</p>
+            <p style={{ padding: '40px 10px', borderTop: '1px solid gray', width: '100%' }}>© ISMA Ltd {new Date().getFullYear()}. All Rights Reserved.</p>
         </PageSizedContainer>
     </FullWidthContainer>
   )
