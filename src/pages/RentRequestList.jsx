552060c87354c547eb824652f85c291166d70615
hirwajeaneric <hirwaminerve25@gmail.com>
2023-06-08 10:58:21 +0200

    Inner page designs over

--- src/pages/RentRequestList.jsx
+++ src/pages/RentRequestList.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Helmet } from 'react-helmet-async'
+import { InnerContainer } from '../components/styled-components/authenticationPages'
+import { HeaderTwo } from '../components/styled-components/generalComponents'
 
 export default function RentRequestList() {
   return (
@@ -8,7 +10,10 @@ export default function RentRequestList() {
         <title>Rent requests</title>
         <meta name="description" content={`List of all rent requests, both sent by me and those sent to me.`} /> 
       </Helmet>
-      RentRequestList
+      <InnerContainer style={{ width: '100%', alignItems:'flex-start', margin: '0', background: 'none', borderTop: 'none' }}>
+        <HeaderTwo style={{ margin: '0', borderBottom: '1px solid rgb(120,116,116, 0.5)', paddingBottom: '10px', width: '100%' }}>Rent Requests</HeaderTwo>
+        
+      </InnerContainer>
     </div>
   )
 }
--- src/pages/TenantInfo.jsx
+++ src/pages/TenantInfo.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Helmet } from 'react-helmet-async'
+import { InnerContainer } from '../components/styled-components/authenticationPages'
+import { HeaderTwo } from '../components/styled-components/generalComponents'
 
 export default function TenantInfo() {
   
@@ -9,7 +11,10 @@ export default function TenantInfo() {
         <title>Tenant info</title>
         <meta name="description" content={`Details for tenant.`} /> 
       </Helmet>
-      TenantInfo
+      <InnerContainer style={{ width: '100%', alignItems:'flex-start', margin: '0', background: 'none', borderTop: 'none' }}>
+        <HeaderTwo style={{ margin: '0', borderBottom: '1px solid rgb(120,116,116, 0.5)', paddingBottom: '10px', width: '100%' }}>Tenant Info</HeaderTwo>
+        
+      </InnerContainer>
     </div>
   )
 }
