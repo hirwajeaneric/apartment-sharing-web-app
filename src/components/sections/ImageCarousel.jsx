b071ef60395e979abcc19e3c2dcfc47463b7cb92
hirwajeaneric <hirwaminerve25@gmail.com>
2023-06-08 17:49:13 +0200

    Image carousel working

--- src/components/sections/ImageCarousel.jsx
+++ src/components/sections/ImageCarousel.jsx
@@ -1,22 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Paper } from '@mui/material';
 import { CustomCarousel } from '../styled-components/generalComponents';
 import { APIS } from '../../utils/APIS';
 
 export default function ImageCarousel({pictures}) {
-  console.log(pictures);
+  const [images, setImages] = useState([]);
+  useEffect(()=> {
+    setImages(pictures);
+  },[pictures])
+ 
   return (
     <CustomCarousel>
-        { pictures.map((picture, index) => <Item key={index} picture={picture} /> )}
+        { images && images.map((image, index) => 
+          <Paper key={index} sx={{ width: '100%' }}>
+            <img style={{ width: '100%' }} src={`${APIS.files.property}${image}`} alt={image} />
+          </Paper>
+        )}
     </CustomCarousel>
-  )
-}
-
-function Item(props) {
-  console.log(props.picture);
-  return (
-    <Paper sx={{ width: '100%' }}>
-        <img style={{ width: '100%' }} src={`${APIS.files.property}${props.picture}`} alt={props.picture} />
-    </Paper>
   )
 }
