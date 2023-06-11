073dc9234abed2c61487c5d6852d0333d6233154
hirwajeaneric <hirwaminerve25@gmail.com>
2023-06-11 21:37:38 +0200

    Join post slice over

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,6 +3,7 @@ import propertyReducer from './features/propertySlice';
 import rentRequestReducer from './features/rentRequestsSlice';
 import joinRequestReducer from './features/joinRequestsSlice';
 import contractReducer from './features/contractSlice';
+import joinPostReducer from './features/joinPostSlice';
 
 export const store = configureStore({
   reducer: {
@@ -10,5 +11,6 @@ export const store = configureStore({
     rentRequest: rentRequestReducer,
     joinRequest: joinRequestReducer,
     contract: contractReducer,
+    joinPost: joinPostReducer,
   },
 });
