34d2e3eaafeaedb3e4cce60497aa3860deb26b41
hirwajeaneric <hirwaminerve25@gmail.com>
2023-06-16 17:38:50 +0200

    Designed the contract details
    Made the contract view even better

--- src/components/forms/ContractDetailsForm.jsx
+++ src/components/forms/ContractDetailsForm.jsx
@@ -47,8 +47,6 @@ export default function ContractDetailsForm() {
     const {signator, userInfo, signature} = props;
     var formData = {};
 
-    console.log(props);
-
     if (signator === 'owner') {
       formData = {
         ownerSignature : signature, 
@@ -68,8 +66,6 @@ export default function ContractDetailsForm() {
         ]
       }
     }
-
-    console.log(formData);
     
     setIsProcessing(true);
     axios.put(APIS.contractApis.update+params.contractId, formData)
@@ -105,15 +101,32 @@ export default function ContractDetailsForm() {
   }
 
   return (
-    <TwoSidedContainer style={{ flexDirection:'column', marginTop: '20px', gap:'20px', width: '100%', background: 'white', padding: '30px', boxShadow: '0 1.5px 5px 0 rgba(0, 0, 0, 0.19)', borderRadius:'5px' }}>
+    <TwoSidedContainer style={{ flexDirection:'column', background: '#ffffcc', marginTop: '20px', gap:'20px', width: '100%', padding: '30px', boxShadow: '0 1.5px 5px 0 rgba(0, 0, 0, 0.19)', borderRadius:'5px' }}>
       
-
       {/* GENERAL DETAILS ****************************************************************************************************************************** */}
 
       <div style={{ display: 'flex', flexDirection:'column', gap: '20px', alignItems:'flex-start', width:'100%'}}>
-        <HeaderThree style={{ margin: '0', fontWeight: '600', color:'green', width: '100%', paddingBottom: '10px', borderBottom: '1px solid green' }}>General Details</HeaderThree>
-        <p style={{ lineHeight: '25px' }}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto animi eligendi reprehenderit numquam ad quibusdam repellendus minima nostrum? Amet doloremque, sit praesentium officia aut nulla saepe nesciunt minus corporis adipisci.
+        <HeaderThree style={{ margin: '0', fontWeight: '600', color:'green', width: '100%', paddingBottom: '10px', borderBottom: '1px solid green' }}>ISAM Rental Agreement</HeaderThree>
+        <p style={{ lineHeight: '25px', marginBottom:'20px', fontSize:'90%' }}>
+          This Rental Agreement is entered into between tenant(s) listed bellow and the onwer.
+          <br/><br/>
+          The parties agree to utilize ISMA as a platform to facilitate the rental of property and the formation of shared living arrangements. 
+          <br/>The App serves as a digital marketplace connecting individuals seeking to rent houses and those looking for housemates to share the rent price.
+          <br/><br/>
+          By engaging with the App and entering into this Agreement, both the Tenant and Owner acknowledge and agree to the terms and conditions outlined herein, 
+          which govern their rights and obligations regarding the rental and shared living arrangement.
+          <br/><br/>
+          It is essential to thoroughly review this Agreement and ensure a clear understanding of its contents before proceeding. 
+          <br/>This Agreement is designed to protect the interests of both parties, provide clarity on the terms of the rental, 
+          and establish a framework for the shared living arrangement facilitated through the App.
+          <br/><br/>
+          Please note that this Agreement is not a substitute for legal advice. <br/>
+          It is recommended that both the Tenant and Owner consult with a qualified attorney to address any specific legal concerns or 
+          questions they may have related to this Agreement or their respective rights and obligations.
+          <br/><br/>
+          By using the App and proceeding with the rental and shared living arrangement, 
+          the Tenant and Owner demonstrate their commitment to following the terms of this Agreement and utilizing the App's functionalities 
+          to facilitate a smooth and transparent process.
         </p>
         <TwoSidedContainer style={{ flexDirection:'row', width: '100%' }}>
           <LeftContainer style={{ flexDirection: 'column', gap: '20px', marginBottom:'20px', justifyContent:'flex-start', alignItems:'flex-start'}}>
