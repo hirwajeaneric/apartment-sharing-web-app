b34fe8209743d2439a3b760dcae1054e9f6e5ca6
hirwajeaneric <hirwaminerve25@gmail.com>
2023-06-22 20:59:12 +0200

    Join request form

--- src/components/forms/JoinRequestForm.jsx
+++ src/components/forms/JoinRequestForm.jsx
@@ -1,37 +1,108 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { FormContainer } from '../styled-components/formsStyledComponent'
 import { Button, InputLabel, MenuItem, Select, TextField } from '@mui/material'
 import { useParams } from 'react-router-dom';
 import { CustomFormControlOne } from '../styled-components/generalComponents';
+import { useDispatch } from 'react-redux';
+import { getJoinRequests } from '../../redux/features/joinRequestsSlice';
+import axios from 'axios';
+import { APIS } from '../../utils/APIS';
+import ResponseComponent from '../sections/ResponseComponent';
 
-export default function JoinRequestForm() {
+export default function JoinRequestForm(props) {
+  // PROPS DESTRUCTURING
+  const { joinPostId } = props;
+
+  // FORM PROCESSING AND RESPONSE PROVISION
+  const [isProcessing, setIsProcessing] = useState(false);
+  const [responseMessage, setResponseMessage] = useState({ message: '', severity: ''});
+  const [open, setOpen] = useState(false);
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setOpen(false);
+  };
+
   const params =  useParams();
+  const dispatch = useDispatch();
+
+  // OTHER STATES
+  const [user, setUser] = useState({});  
   const [formData, setFormData] = useState({
-    propertyId: params.id,
-    joinPost: '',
+    propertyId: '',
     requestingUserId: '',
     fullName: '',
     email: '',
     phone: '',
     gender: '',
-    age: '',
+    age: 0,
     comment: '',
+    nationalId: '',
+    passportNumber: '',
+    mightNeedToShare: '',
   });
+
+  const resetFields = () => {
+    setFormData({
+      gender: '',
+      age: 0,
+      comment: '',
+      nationalId: '',
+      passportNumber: '',
+      mightNeedToShare: '',
+    });
+  }
+
+  useEffect(()=> {
+    setUser(JSON.parse(localStorage.getItem('usrInfo')));
+    setFormData({
+      ...formData, 
+      fullName: user.fullName,
+      email: user.email,
+      phone: user.phone,
+      nationalId: user.nationalId,
+      passportNumber: user.passportNumber,
+      propertyId: params.id,
+    })
+  },[formData, user, params])
 
   const handleFormInputs = event => {
     setFormData({ ...formData, [event.target.name] : event.target.value });
-    console.log(formData);
   }
 
   const submitRequest = e => {
     e.preventDefault();
 
-    console.log(formData);
+    formData.requestingUserId = user.id;
+    formData.joinPost = joinPostId;
+
+    setIsProcessing(true);
+    axios.post(APIS.joinRequestApis.add, formData)
+    .then(response => {
+      setTimeout(() => {
+        if (response.status === 201) {
+          setIsProcessing(false);
+          setResponseMessage({ message: 'Join request sent', severity:'success'});
+          setOpen(true);
+          resetFields();
+        }
+        dispatch(getJoinRequests(response.data.joinRequest.requestingUserId));
+      },3000);
+    })
+    .catch(error => {
+      if (error.response && error.response.status >= 400 && error.response.status <= 500) {
+        setIsProcessing(false);
+        setResponseMessage({ message: error.response.data.msg, severity:'error'})
+        setOpen(true);
+      }
+    })
   }
 
   return (
     <FormContainer onSubmit={submitRequest} style={{ flexDirection:'column', marginTop: '20px' }}>
       <TextField 
+        disabled 
         variant='outlined' 
         style={{ width: '100%' }} 
         label='Full Name' 
@@ -42,22 +113,24 @@ export default function JoinRequestForm() {
         onChange={handleFormInputs}
       />
       <TextField 
+        disabled 
         variant='outlined' 
         style={{ width: '100%' }} 
         label='Email' 
         id='email' 
         size='small' 
-        value={formData.email || ''} 
+        value={formData.email  || ''} 
         name='email' 
         onChange={handleFormInputs}
       />
       <TextField 
+        disabled 
         variant='outlined' 
         style={{ width: '100%' }} 
         label='Phone' 
         id='phone' 
         size='small' 
-        value={formData.phone || ''} 
+        value={formData.phone  || ''} 
         name='phone' 
         onChange={handleFormInputs}
       />
@@ -76,7 +149,7 @@ export default function JoinRequestForm() {
           </MenuItem>
           <MenuItem value={'male' || ''}>Male</MenuItem>
           <MenuItem value={'female' || ''}>Female</MenuItem>
-          <MenuItem value={'Prefer not to say' || ''}>Prefer not to say</MenuItem>
+          <MenuItem value={'prefer not to say' || ''}>Prefer not to say</MenuItem>
         </Select>
       </CustomFormControlOne>
       <TextField 
@@ -90,6 +163,24 @@ export default function JoinRequestForm() {
         name='age' 
         onChange={handleFormInputs}
       />
+      <CustomFormControlOne sx={{ width: '100%' }} size='small'>
+        <InputLabel id="gender">Will you need to share the property?</InputLabel>
+        <Select 
+          labelId="mightNeedToShare" 
+          id="mightNeedToShare" 
+          name='mightNeedToShare' 
+          value={formData.mightNeedToShare || ''} 
+          onChange={handleFormInputs} 
+          label="Will you need to share the property?"
+        >
+          <MenuItem value="">
+              <em>None</em>
+          </MenuItem>
+          <MenuItem value={'Yes' || ''}>Yes</MenuItem>
+          <MenuItem value={'No' || ''}>No</MenuItem>
+          <MenuItem value={"Don't know yet" || ''}>I don't know yet</MenuItem>
+        </Select>
+      </CustomFormControlOne>
       <TextField 
         id="outlined-multiline-static" 
         style={{ width: '100%' }} 
@@ -100,7 +191,23 @@ export default function JoinRequestForm() {
         value={formData.comment || ''} 
         onChange={handleFormInputs} 
       />
-      <Button style={{ marginTop: '20px' }} variant='contained' color='primary' size='small' type='submit'>Submit Request</Button>
+      
+        <div style={{ display: 'flex', flexWrap: 'nowrap', justifyContent:'space-between', alignItems:'center', width: '100%' }}>
+          {!isProcessing && 
+            <Button type='submit' variant='contained' size='small' color='primary'>SUBMIT</Button>
+          }
+          {isProcessing && 
+            <Button type='submit' variant='contained' size='medium' color='primary' disabled>PROCESSING...</Button>
+          }
+          <Button type='cancel' variant='contained' color='secondary' size='small' onClick={resetFields}>CANCEL</Button>
+        </div>
+
+        <ResponseComponent 
+          message={responseMessage.message} 
+          severity={responseMessage.severity}
+          open={open} 
+          handleClose={handleClose} 
+        />
     </FormContainer>
   )
 }
--- src/pages/PropertyDetailsHome.jsx
+++ src/pages/PropertyDetailsHome.jsx
@@ -182,7 +182,7 @@ export default function PropertyDetailsHome() {
               
               {
                 user !== null && selectedProperty.status === 'For Join' && selectedProperty.ownerId !== user.id && !postedByMe ? 
-                <JoinRequestForm /> : 
+                <JoinRequestForm joinPostId={joinPost._id} /> : 
                 <></>
               }
             </div>
