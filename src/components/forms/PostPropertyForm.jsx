88f03a92cc5c35d35ee32f5994c6ea7978c75498
hirwajeaneric <hirwaminerve25@gmail.com>
2023-06-27 15:52:47 +0200

    Refactoring

--- src/components/forms/PostPropertyForm.jsx
+++ src/components/forms/PostPropertyForm.jsx
@@ -5,14 +5,12 @@ import Snackbar from '@mui/material/Snackbar';
 import MuiAlert from '@mui/material/Alert';
 import axios from 'axios';
 import { APIS } from '../../utils/APIS';
-import { useParams } from 'react-router-dom';
 
 const Alert = React.forwardRef(function Alert(props, ref) {
   return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
 });
 
 export default function PostPropertyForm() {
-  const params = useParams();
   const [userData, setUserData] = useState({});
   const [pictures, setPictures] = useState([]);
   const [formData, setFormData] = useState({
