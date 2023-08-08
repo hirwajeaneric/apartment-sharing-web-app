011249e796fde77445b93f370154c21ed0ffe4c9
hirwajeaneric <hirwaminerve25@gmail.com>
2023-08-08 17:11:51 +0200

    Report

--- src/pages/ContractDetails.jsx
+++ src/pages/ContractDetails.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { Helmet } from 'react-helmet-async'
 import { InnerContainer } from '../components/styled-components/authenticationPages'
 import { HeaderTwo } from '../components/styled-components/generalComponents'
 import ContractDetailsForm from '../components/forms/ContractDetailsForm'
-import { Button } from '@mui/material'
+import { Button } from '@mui/material';
+import { useReactToPrint } from 'react-to-print';
+import { ComponentToPrint } from '../components/sections/ComponentToPrint';
 
 export default function ContractDetails() {
+  const componentRef = useRef();
+  const handlePrint = useReactToPrint({
+      content: () => componentRef.current
+  });
+
   return (
     <div>
       <Helmet>
@@ -15,11 +22,12 @@ export default function ContractDetails() {
       <InnerContainer style={{ width: '100%', alignItems:'flex-start', margin: '0', background: 'none', borderTop: 'none' }}>
         <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems:'center', width: '100%', borderBottom: '1px solid rgb(120,116,116, 0.5)', paddingBottom: '10px' }}>
           <HeaderTwo style={{ margin: '0' }}>Contract Details</HeaderTwo>
-          <Button variant='contained' size='small' color='secondary' >Print</Button>
+          <Button variant='contained' size='small' color='secondary' onClick={handlePrint}>Print</Button>
         </div>
 
         <ContractDetailsForm />
-        
+        <ComponentToPrint ref={componentRef} />
+
       </InnerContainer>
     </div>
   )
